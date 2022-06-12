562741dd9d20b7401f0cc348ba938129618f037f
Jonathan Matsumoto <61896186+jonkmatsumo@users.noreply.github.com>
2022-06-12 16:13:15 -0700

    load charts and read contracts in from json

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
+import json
 import matplotlib.pyplot as plt
 import streamlit as st
+from time import sleep
 from prophetPreprocessor import ProphetPreprocessor
 
 api_key = 'ckey_70a8a6bf18464adc99f3057a1ac'
 
 def main():
-    st.sidebar.title("Parameters")
-    collection = st.sidebar.selectbox("Choose Collection", ("BAYC", "MAYC"))
-
-    st.title("BridXe Market Intelligence Platform")
     prophetpre = ProphetPreprocessor(api_key)
 
-    mayc = prophetpre.load_data("0x60E4d786628Fea6478F785A6d7e704777c86a7c6", "floor_7d")
-    st.dataframe(mayc)
+    @st.cache(persist=True)
+    def load_data():
+        with open('contracts.json', 'r') as f:
+            contracts = json.load(f)
+
+        keys = [c for c in contracts.keys()]
+
+        return contracts, keys
+
+    contracts, keys = load_data()
+    st.sidebar.title("Parameters")
+    with st.sidebar.form("parameters"):
+        collection = st.selectbox("Choose Collection", keys)
+        metric = st.selectbox("Choose metric", ("floor_7d", "avg_volume_1d", "volume_1d", "sold_1d"))
+        submitted = st.form_submit_button("Submit")
+
+    if submitted:
+        st.title(collection)
+        ts = prophetpre.load_data(contracts[collection], metric)
+        plt.plot(ts['ds'], ts['y'], color='red')
+        st.pyplot(plt)
 
 
 if __name__ == '__main__':
